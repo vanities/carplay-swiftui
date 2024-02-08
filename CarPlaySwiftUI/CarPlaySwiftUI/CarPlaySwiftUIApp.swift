806dc37e8291b57b96c278ff5ed31f02916a782a
Adam Mischke <mischkeaa@gmail.com>
2024-02-08 08:46:40 -0600

    initial commit

---
+++ CarPlaySwiftUI/CarPlaySwiftUI/CarPlaySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarPlaySwiftUIApp.swift
+//  CarPlaySwiftUI
+//
+//  Created by Adam Mischke on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CarPlaySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
